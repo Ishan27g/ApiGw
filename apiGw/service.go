67f709a4590828d51bf72d27d60083be28b95659
Ishan Goyal <63078216+Ishan27g@users.noreply.github.com>
2022-02-27 15:31:40 +1100

    adds transport rt and tests (#3)

--- apiGw/service.go
+++ apiGw/service.go
@@ -1,31 +1,53 @@
 package apiGw
 
 import (
-	"log"
-	"net/http/httputil"
-	"net/url"
+	"io"
+	"net"
+	"net/http"
+	"time"
 )
+
+var defaultTransport http.RoundTripper = &http.Transport{
+	DialContext: (&net.Dialer{
+		Timeout:   10 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}).DialContext,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+}
+type RTPxy struct {}
 
 type service struct {
 	upstream upstream
-	proxy    *httputil.ReverseProxy
+	pxy RTPxy
 }
-
 func newService(upstream upstream) service {
-	proxy, err := newProxy(upstream.Addr)
-	if err != nil {
-		log.Fatal("bad host name")
-	}
 	return service{
 		upstream: upstream,
-		proxy:    proxy,
+		pxy: RTPxy{},
 	}
 }
-func newProxy(dst string) (*httputil.ReverseProxy, error) {
-	if url, err := url.Parse(dst); err == nil {
-		proxy := httputil.NewSingleHostReverseProxy(url)
-		return proxy, err
-	} else {
-		return nil, err
+
+func (p *RTPxy) RoundTrip(host string, rw http.ResponseWriter, req *http.Request) int {
+	outReq, _ := http.NewRequest(req.Method, host + req.RequestURI, req.Body)
+	for key, value := range req.Header {
+		for _, v := range value {
+			outReq.Header.Add(key, v)
+		}
 	}
+	res, err := defaultTransport.RoundTrip(outReq)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadGateway)
+		return http.StatusBadGateway
+	}
+	for key, value := range res.Header {
+		for _, v := range value {
+			rw.Header().Add(key, v)
+		}
+	}
+	rw.WriteHeader(res.StatusCode)
+	io.Copy(rw, res.Body)
+	res.Body.Close()
+	return res.StatusCode
 }
